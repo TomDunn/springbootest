4caa0b36bf9b73dd4f0e556c109102b09ddf5fc0
tomdunn <tidunn13@gmail.com>
2015-07-18 20:42:01 -0700

    Use lombok to inject logger instances

--- src/main/java/demo/controller/Home.java
+++ src/main/java/demo/controller/Home.java
@@ -1,5 +1,6 @@
 package demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,18 +9,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.Map;
 
 /**
- * Created by tom on 7/18/15.
+ * Renders index page
  */
 @Controller
+@Slf4j
 public class Home {
 
-    @Value("${application.author}")
-    private String appAuthor = "default";
+    @Value("${application.author}") private String appAuthor = "default";
     private static final String builtByMessage = "Built by: ";
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(final Map<String, Object> model) {
         model.put("message", builtByMessage + appAuthor);
+        log.info(String.format("Author is [%s]", appAuthor));
         return "pages/index";
     }
 }
